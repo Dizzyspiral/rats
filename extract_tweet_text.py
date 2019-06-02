9c87a5b9631b2e2beabdf4d61838f574240a5016
Courtney Webster <contact@courtneywebster.com>
2019-06-02 19:12:54 -0400

    Fixed unicode errors preventing tweets from writing to file

--- extract_tweet_text.py
+++ extract_tweet_text.py
@@ -1,10 +1,12 @@
-# Author: Courtney Webster
+# Author: Dizzyspiral
 # Purpose: transform the twitter_samples data into something that can be 
 # classified by the nltk-classifier. We're only interested in the text
 # field of the JSON that makes up the tweets, so we want to read each 
 # tweet in, get the text field, and output it to a unique filename. We
 # want the output format to be identical to what will come out of the 
 # twitter-scraper.
+# 
+# Requires python 3
 
 import json
 import sys
@@ -25,13 +27,9 @@ def main():
         for raw_tweet in tweets_file:
             tweet_count += 1
             tweet = json.loads(raw_tweet)
-            with open(output_dir + os.sep + tweet_basename + str(tweet_count) + file_extension, 'wt') as output_file:
-                try:
-                    output_file.write(tweet["text"])
-                except:
-                    # Occasionally, a tweet doesn't have a text field. This
-                    # is due to retweets I think. We just let them fail silently.
-                    pass
+
+            with open(output_dir + os.sep + tweet_basename + str(tweet_count) + file_extension, 'wt', encoding='utf-8') as output_file:
+                output_file.write(tweet["text"])
 
 if __name__ == '__main__':
     main()
